fb663da9dee6f1ea0b179e6f17f25c1f6c0f6aa4
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2026-02-21 13:42:27 +0200

    refactor selenium scripts to consolidate ChromeDriver setup and enhance table data extraction

--- flaskr/tools/selenium/selenimue_auto.py
+++ flaskr/tools/selenium/selenimue_auto.py
@@ -1,8 +1,8 @@
 import os
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
